8259bc4f7aa61394abd4989e58eb9cf3aaf1d857
Grant Barnes <gfbarnes78@gmail.com>
2021-05-15 22:14:14 -0500

    adding playback of sound on flash cards

--- src/app/pages/flash/word/word.component.ts
+++ src/app/pages/flash/word/word.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HttpService } from '../../../shared/services/http/http.service';
 import { Card } from '../card';
 
 @Component({
@@ -10,10 +11,27 @@ export class WordComponent implements OnInit {
   @Input() flipped: boolean = true;
   @Input() card: Card = { id: 0, word: '', translation: '', audio: new Blob() };
   @Output() showSectionEvent = new EventEmitter<string>();
+  audio: any;
 
-  constructor() {}
+  constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    this.audio = null;
+    this.httpService.getAudio('/api/audio/' + this.card.id).subscribe({
+      next: (blob: Blob) => {
+        this.audio = {
+          play: () => new Audio(URL.createObjectURL(blob)).play(),
+        };
+      },
+      error: () => {},
+    });
+  }
+
+  playAudio(): void {
+    if (this.audio) this.audio.play();
+  }
 
   showSection(): void {
     this.showSectionEvent.emit('word');
