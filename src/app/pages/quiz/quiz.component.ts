385e0a73ddc3d84c2bf5cc8a1574ac8d382e2570
Grant Barnes <gfbarnes78@gmail.com>
2021-06-12 15:06:10 -0500

    adding the option to choose how many answers on the quiz

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -12,6 +12,8 @@ export class QuizComponent implements OnInit {
   all_card_ids: number[] = [];
   correct_idx: number = 0;
   cards: Card[] = [];
+  answer_count: number = 3;
+  answer_options: number[] = [2, 3, 4, 5, 6];
 
   constructor(private httpService: HttpService) {}
 
@@ -24,20 +26,25 @@ export class QuizComponent implements OnInit {
 
   nextQuestion(): void {
     if (!this.all_card_ids.length) return;
-    if (this.all_card_ids.length < 5) return;
+    if (this.all_card_ids.length < this.answer_count) return;
     let indexes = new Set();
     let card_ids = [];
-    while (indexes.size !== 4) {
+    while (indexes.size !== this.answer_count) {
       const idx = Math.floor(Math.random() * this.all_card_ids.length);
       if (!indexes.has(idx)) {
         indexes.add(idx);
         card_ids.push(this.all_card_ids[idx]);
       }
     }
-    this.correct_idx = Math.floor(Math.random() * 4);
+    this.correct_idx = Math.floor(Math.random() * this.answer_count);
 
     this.httpService
       .post('/api/cards/bulk', card_ids)
       .subscribe((data: any) => (this.cards = data));
+  }
+
+  setAnswerCount(count: number): void {
+    this.answer_count = count;
+    this.nextQuestion();
   }
 }
