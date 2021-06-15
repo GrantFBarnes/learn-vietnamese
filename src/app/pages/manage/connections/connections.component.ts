904d2bf106e8617effa0a25b411bb3abf8e81c09
Grant Barnes <gfbarnes78@gmail.com>
2021-06-14 19:55:13 -0500

    updating connections page to display connections by ip

--- src/app/pages/manage/connections/connections.component.ts
+++ src/app/pages/manage/connections/connections.component.ts
@@ -11,33 +11,28 @@ import { Connection } from '../../../shared/interfaces/connection';
 export class ConnectionsComponent implements OnInit {
   authorized: boolean = false;
 
-  connections: Connection[] = [];
-
-  ip_connections: { [ip: string]: number } = {};
+  total_connections: number = 0;
   total_ips: number = 0;
-
-  day_connections: { [time: string]: number } = {};
-  total_days: number = 0;
+  ip_connections: { [ip: string]: Connection[] } = {};
+  ip_show: { [ip: string]: boolean } = {};
+  ip_show_all: boolean = false;
 
   constructor(private httpService: HttpService) {}
 
   getConnections(): void {
     this.httpService.get('/api/dump/connections').subscribe((data: any) => {
-      this.connections = data;
-
       for (let i in data) {
         const connection = data[i];
-
         const ip = connection.ip;
-        if (!this.ip_connections[ip]) this.ip_connections[ip] = 0;
-        this.ip_connections[ip]++;
 
-        const day = new Date(connection.time).toDateString();
-        if (!this.day_connections[day]) this.day_connections[day] = 0;
-        this.day_connections[day]++;
+        if (!this.ip_connections[ip]) this.ip_connections[ip] = [];
+        this.ip_connections[ip].push(connection);
+
+        if (!this.ip_show[ip]) this.ip_show[ip] = false;
+
+        this.total_connections++;
       }
       this.total_ips = Object.keys(this.ip_connections).length;
-      this.total_days = Object.keys(this.day_connections).length;
     });
   }
 
@@ -60,5 +55,16 @@ export class ConnectionsComponent implements OnInit {
       next: () => this.authorize(),
       error: () => (this.authorized = false),
     });
+  }
+
+  toggleIPShowAll(): void {
+    this.ip_show_all = !this.ip_show_all;
+    for (let ip in this.ip_show) {
+      this.ip_show[ip] = this.ip_show_all;
+    }
+  }
+
+  toggleIPShow(ip: string): void {
+    this.ip_show[ip] = !this.ip_show[ip];
   }
 }
